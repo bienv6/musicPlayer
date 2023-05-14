1449bb5b3ff750fb7227cae6dfe993d24a559d03
vernet bien-aime <vernet0312@gmail.com>
2023-05-13 21:45:01 -0400

    created comments component

---
+++ src/components/Comments.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+const  Comments = (props) => {
+    let currentTrack = props.currentTrack;
+
+  const [ comments ,setComments] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/rest/comment/${currentTrack.id}`)
+            .then((response) => {
+            setComments(response.data);
+            console.log(`comments on this song are: ${response.data}`)
+        })
+            .catch(error => console.log("Error Loading Comments"))
+
+    }, [currentTrack])
+
+    return (
+        <>
+            {comments.map((comment, index) => (
+                <div key={index}>
+                    <div>
+                        <span>{comment.commentedBy}</span> : {comment.comment}
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default Comments;
