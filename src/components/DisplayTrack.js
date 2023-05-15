04ecadcf5eb3dec84cc23548abbb1a94c25b7d9b
vernet bien-aime <vernet0312@gmail.com>
2023-05-15 08:15:39 -0400

    likes button will disable once clicked and activate once song is changed.

--- src/components/DisplayTrack.js
+++ src/components/DisplayTrack.js
@@ -1,26 +1,24 @@
-import { BsMusicNoteBeamed } from 'react-icons/bs';
+import {BsMusicNoteBeamed} from 'react-icons/bs';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import {useEffect, useState} from "react";
 
 const DisplayTrack = ({
-  currentTrack,
-  audioRef,
-  setDuration,
-  progressBarRef,
-  handleNext,
-}) => {
-  const onLoadedMetadata = () => {
-    const seconds = audioRef.current.duration;
-    setDuration(seconds);
-    progressBarRef.current.max = seconds;
-  };
-  const [liked,setLiked] = useState(false);
+                          currentTrack,
+                          audioRef,
+                          setDuration,
+                          progressBarRef,
+                          handleNext,
+                      }) => {
+    const onLoadedMetadata = () => {
+        const seconds = audioRef.current.duration;
+        setDuration(seconds);
+        progressBarRef.current.max = seconds;
+    };
+    const [liked, setLiked] = useState(false);
 
-  const handleLikesClick = (e, liked) =>
-
-    {
+    const handleLikesClick = (liked) => {
         // MAKE API CALL TO UPDATE SONG SERVICE LIKES
-        if(liked !== true){
+        if (liked !== true) {
             currentTrack.likesCount++;
             console.log(`this is the current likes count ${currentTrack.likesCount}`)
             console.log(`like button is now disabled: ${e.target}`)
@@ -30,37 +28,42 @@ const DisplayTrack = ({
 
     };
 
+    useEffect(() => {
+        setLiked(false);
+        console.log("like button is enabled");
+    }, [currentTrack.title])
 
-
-  return (
-    <div>
-      <audio
-        src={currentTrack.src}
-        ref={audioRef}
-        onLoadedMetadata={onLoadedMetadata}
-        onEnded={handleNext}
-      />
-      <div className="text">
-        <p className="title">{currentTrack.title}
-            <span className="likes1"><button onClick={handleLikesClick} disabled={liked} name="likedBtn"><ThumbUpIcon/></button> {currentTrack.likesCount}</span>
-        </p>
-        <p>{currentTrack.author}</p>
-      </div>
-      <div className="audio-info">
-        <div className="audio-image">
-          {currentTrack.thumbnail ? (
-            <img src={currentTrack.thumbnail} alt="audio avatar" />
-          ) : (
-            <div className="icon-wrapper">
-              <span className="audio-icon">
-                <BsMusicNoteBeamed />
-              </span>
+    return (
+        <div>
+            <audio
+                src={currentTrack.src}
+                ref={audioRef}
+                onLoadedMetadata={onLoadedMetadata}
+                onEnded={handleNext}
+            />
+            <div className="text">
+                <p className="title">{currentTrack.title}
+                    <span className="likes1"><button onClick={handleLikesClick} disabled={liked}
+                                                     name="likedBtn"><ThumbUpIcon/></button>
+                        {currentTrack.likesCount}</span>
+                </p>
+                <p>{currentTrack.author}</p>
             </div>
-          )}
-        </div>
+            <div className="audio-info">
+                <div className="audio-image">
+                    {currentTrack.thumbnail ? (
+                        <img src={currentTrack.thumbnail} alt="audio avatar"/>
+                    ) : (
+                        <div className="icon-wrapper">
+              <span className="audio-icon">
+                <BsMusicNoteBeamed/>
+              </span>
+                        </div>
+                    )}
+                </div>
 
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 export default DisplayTrack;
